bb07e414ff8bb9cfcaa99ef485593e255342d029
Damian Puś <35109367+DragHem@users.noreply.github.com>
2022-07-15 09:49:52 +0200

    (Add) Login Form

--- src/App.js
+++ src/App.js
@@ -4,30 +4,6 @@ import "./App.css";
 import LoginForm from "./components/LoginForm";
 
 function App() {
-  const [email, setEmail] = useState("test@tes.com");
-  const [password, setPassword] = useState("Has@12123321123");
-  const [userData, setUserData] = useState({});
-
-  const submitHandler = async (e) => {
-    e.preventDefault();
-
-    const resp = await fetch("http://192.168.0.125:8000/api/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify({
-        email: email,
-        password: password,
-      }),
-    });
-
-    const data = await resp.json();
-    setUserData(data);
-    console.log(data);
-  };
-
   return (
     <div className="App">
       <LoginForm />
