a041e8fc1b986529aab21e654c69be83ad291ff4
Damian Puś <35109367+DragHem@users.noreply.github.com>
2022-08-03 11:48:42 +0200

    (Edit) Search

--- src/components/Search.js
+++ src/components/Search.js
@@ -7,7 +7,7 @@ const Search = () => {
   const [inputValue, setInputValue] = useState("");
   const [selectOption, setSelectOption] = useState("tags");
 
-  const { isLoading, error, sendRequest } = useHttp();
+  const { sendRequest } = useHttp();
 
   const searchHandler = async (e) => {
     e.preventDefault();
@@ -54,22 +54,6 @@ const Search = () => {
               <option value="users">Users</option>
             </select>
           </form>
-          {/*<button onClick={clickHandler} className="btn btn-square">*/}
-          {/*  <svg*/}
-          {/*    xmlns="http://www.w3.org/2000/svg"*/}
-          {/*    className="h-6 w-6"*/}
-          {/*    fill="none"*/}
-          {/*    viewBox="0 0 24 24"*/}
-          {/*    stroke="currentColor"*/}
-          {/*  >*/}
-          {/*    <path*/}
-          {/*      strokeLinecap="round"*/}
-          {/*      strokeLinejoin="round"*/}
-          {/*      strokeWidth="2"*/}
-          {/*      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"*/}
-          {/*    />*/}
-          {/*  </svg>*/}
-          {/*</button>*/}
         </div>
       </div>
     </div>
