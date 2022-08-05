315ec03031ba64d3aaaa395fbc290341281ba54a
SamSepi0l-H3ll0-Fi3ND <kubafigzal@gmail.com>
2022-08-05 11:31:29 +0200

    try1

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -66,6 +66,25 @@ const Nav = () => {
             <span className="hidden sm:block">My posts</span>
           </Link>
         </li>
+        <li>
+          <Link to="myposts">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="w-4 h-4 mr-2 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z"
+              ></path>
+            </svg>
+
+            <span className="hidden sm:block">Messenger</span>
+          </Link>
+        </li>
       </ul>
       {!token && (
         <Link to="/register">
